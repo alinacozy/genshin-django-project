d3c2791bb176dc51307c270c959afb9e450bc202
alinacozy <alinakoroleva2307@gmail.com>
2026-02-18 19:34:49 +0400

    registration & password change

--- GenshinProject/main/urls.py
+++ GenshinProject/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home' ),
     path('about/', views.about,  name='about'),
-    path('profile/', views.profile,  name='profile')
+    path('profile/', views.profile,  name='profile'),
+    path('register/', views.RegisterView.as_view(),  name='register')
 ]
--- GenshinProject/main/views.py
+++ GenshinProject/main/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.views.generic import CreateView, FormView
+
+from .forms import RegisterForm
+
 
 def index(request):
      data = {
@@ -19,3 +24,14 @@ def about(request):
 @login_required
 def profile(request):
     return render(request, 'main/profile.html')
+
+class RegisterView(FormView):
+    form_class = RegisterForm
+    template_name = 'registration/register.html'
+    success_url = '/profile'
+
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
