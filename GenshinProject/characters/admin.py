db61ba409fd5ccea3695a1908543299243dbe431
alinacozy <alinakoroleva2307@gmail.com>
2026-02-19 18:08:31 +0400

    add new models for materials

--- GenshinProject/characters/admin.py
+++ GenshinProject/characters/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Character, WeeklyMaterial, UserCharacter
+from .models import *
 
 admin.site.register(Character)
 admin.site.register(WeeklyMaterial)
 admin.site.register(UserCharacter)
+admin.site.register(BossMaterial)
+admin.site.register(TalentMaterial)
+admin.site.register(Specialty)
+admin.site.register(Stone)
+admin.site.register(Mob)
+admin.site.register(MobMaterial)
+
--- GenshinProject/characters/models.py
+++ GenshinProject/characters/models.py
@@ -22,11 +22,55 @@ class ElementChoices(models.TextChoices):
     PYRO = 'P', 'Пиро'
     CRYO = 'C', 'Крио'
 
+class WeekChoices(models.IntegerChoices):
+    MONDAY = 1, 'Понедельник'
+    TUESDAY = 2, 'Вторник'
+    WEDNESDAY = 3, 'Среда'
+
 class Character(models.Model):
     name = models.CharField("Имя", max_length=100)
     region = models.CharField("Регион", max_length=2, choices=RegionChoices.choices)
     element = models.CharField("Элемент", max_length=1, choices=ElementChoices.choices)
     weekly_material = models.ForeignKey('WeeklyMaterial', verbose_name="Еженедельный материал", on_delete=models.PROTECT, null=True)
+    boss_material = models.ForeignKey('BossMaterial', verbose_name="Материал босса",
+                                        on_delete=models.PROTECT, null=True)
+    talent_weekday = models.IntegerField(
+        choices=WeekChoices.choices,
+        verbose_name='День недели талантов',
+        help_text='День недели, когда фармятся книги талантов',
+        null = True
+    )
+    specialty = models.ForeignKey('Specialty', verbose_name="Диковинка",
+                                      on_delete=models.PROTECT, null=True)
+
+    mob = models.ForeignKey(  # храним только имя моба
+        'Mob',
+        verbose_name="Моб (материалы)",
+        on_delete=models.PROTECT,
+        help_text="Выберите моба -> получите все его материалы",
+        null = True
+    )
+
+    def get_mob_materials(self):
+        """Получить все 3 материала моба"""
+        return self.mob.materials.order_by('rarity')
+
+    def get_talent_materials(self):
+        """
+        Возвращает все 3 книги талантов этого персонажа
+        """
+        return TalentMaterial.objects.filter(
+            region=self.region,
+            weekday=self.talent_weekday
+        ).order_by('rarity')
+
+    def get_stones(self):
+        """
+        Возвращает необходимые камни возвышения для этого персонажа
+        """
+        return Stone.objects.filter(
+            element=self.element
+        ).order_by('rarity')
 
     def __str__(self):
         return self.name
@@ -52,6 +96,105 @@ class WeeklyMaterial(models.Model):
         verbose_name_plural = 'Еженедельные материалы'
 
 
+class BossMaterial(models.Model):
+    name = models.CharField("Название", max_length=200, db_index=True)
+    boss_name = models.CharField("Название босса", max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Материал босса'
+        verbose_name_plural = 'Материалы босса'
+
+
+class TalentMaterial(models.Model):
+    name = models.CharField("Название", max_length=200, db_index=True)
+    region = models.CharField("Регион", max_length=2, choices=RegionChoices.choices)
+    rarity = models.IntegerField(
+        validators=[
+            MinValueValidator(2),
+            MaxValueValidator(4)
+        ],
+        help_text="Редкость книги может быть от 2 до 4 звезд",
+        verbose_name="Редкость"
+    )
+    weekday=models.IntegerField(
+        choices=WeekChoices.choices,
+        verbose_name='День недели'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Книга талантов'
+        verbose_name_plural = 'Книги талантов'
+
+
+class Specialty(models.Model):
+    name = models.CharField("Название", max_length=200, db_index=True)
+    region = models.CharField("Регион", max_length=2, choices=RegionChoices.choices)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Диковинка'
+        verbose_name_plural = 'Диковинки'
+
+
+class Stone(models.Model):
+    name = models.CharField("Название", max_length=200, db_index=True)
+    element = models.CharField("Элемент", max_length=1, choices=ElementChoices.choices)
+    rarity = models.IntegerField(
+        validators=[
+            MinValueValidator(2),
+            MaxValueValidator(5)
+        ],
+        help_text="Редкость камня может быть от 2 до 5 звезд",
+        verbose_name="Редкость"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Камень'
+        verbose_name_plural = 'Камни'
+
+
+class Mob(models.Model):
+    name = models.CharField("Название моба", max_length=200, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Моб'
+        verbose_name_plural = 'Мобы'
+
+
+class MobMaterial(models.Model):
+    name = models.CharField("Название", max_length=200, db_index=True)
+    mob_name = models.ForeignKey(Mob, on_delete=models.CASCADE, verbose_name="Название моба", max_length=200, db_index=True)
+    rarity = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(3)
+        ],
+        help_text="Редкость материалов с мобов может быть от 1 до 3 звезд",
+        verbose_name="Редкость"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Материал моба'
+        verbose_name_plural = 'Материалы мобов'
+
+
 class UserCharacter(models.Model):
     name = models.ForeignKey("Character", verbose_name="Имя",
                                         on_delete=models.PROTECT, null=True)
@@ -60,7 +203,8 @@ class UserCharacter(models.Model):
             MinValueValidator(1),
             MaxValueValidator(90)
         ],
-        help_text="Уровень должен быть от 1 до 90"
+        help_text="Уровень должен быть от 1 до 90",
+        verbose_name='Уровень'
     )
     is_ascended = models.BooleanField(default=False, verbose_name='Возвышен')
     talent_levels = models.JSONField(default=list, verbose_name='Уровни талантов')
